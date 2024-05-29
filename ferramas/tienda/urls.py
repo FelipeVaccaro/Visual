5c2d419539ae831dc7a6758d713e0d9139b827b1
CETECOM_AVARAS <cetecom_avaras@duoc.cl>
2024-05-29 14:54:22 -0400

    arreglos url:)

--- ferramas/tienda/urls.py
+++ ferramas/tienda/urls.py
@@ -1,21 +1,20 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
+from tienda.views import inicio, inicio_sesion, registro, home, nosotros, validacion_login, registrar_usuario, error_404, cierre_sesion, catalogo_productos
 
 app_name="tienda"
 
 urlpatterns = [
-    path('', views.home, name="index"),
-    path('catalogo/', views.catalogo_productos),
-    path('inicio/', views.inicio),
-    path('inicio-sesion/', views.inicio_sesion, name='inicio-sesion'),
-    path('cierre-sesion/', views.cierre_sesion, name='cierre-sesion'), 
-    path('registro/', views.registro),
-    path('nosotros/', views.nosotros),
-    path('404/', views.error_404),
-    path('validacion-login/', views.validacion_login), #el loginpage usa validacion-login para autenticar al usuario del form
-    path('registrar-usuario/', views.registrar_usuario),
-    # path("agregar/<int:producto_id>/", views.agregar_producto, name ="agregar"),
-    # path("eliminar/<int:producto_id>/", views.eliminar_producto, name ="eliminar"),
-    # path("restar/<int:producto_id>/", views.restar_producto, name ="restar"),
-    # path("limpiar/", views.limpiar_carro, name ="limpiar"),
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('catalogo/', catalogo_productos),
+    path('inicio/', inicio),
+    path('inicio-sesion/', inicio_sesion, name='inicio-sesion'),
+    path('registro/', registro),
+    path('nosotros/', nosotros),
+    path('404/', error_404),
+    path('validacion-login', validacion_login), #el loginpage usa validacion-login para autenticar al usuario del form
+    path('registrar-usuario', registrar_usuario),
+    path('cierre-sesion', cierre_sesion, name='cierre-sesion'), 
 ]
